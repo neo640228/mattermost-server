fdf587205be47a48475fa018734c527968447d40
JoramWilander <jwawilander@gmail.com>
2018-06-12 15:34:27 -0400

    Fix AtRestEncryptKey default

--- model/config.go
+++ model/config.go
@@ -709,7 +709,7 @@ func (s *SqlSettings) SetDefaults() {
 		s.DataSourceSearchReplicas = []string{}
 	}
 
-	if s.AtRestEncryptKey == nil {
+	if s.AtRestEncryptKey == nil || len(*s.AtRestEncryptKey) == 0 {
 		s.AtRestEncryptKey = NewString(NewRandomString(32))
 	}
 
